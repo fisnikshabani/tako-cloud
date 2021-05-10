fa6337c3b6ef5d4b939d870701316fec576be3b6
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-05-10 14:28:02 +0200

    Added Order.java and orderForm.html

--- src/main/java/sia/takocloud/controller/DesignTacoController.java
+++ src/main/java/sia/takocloud/controller/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.takocloud.domain.Ingredient;
 import sia.takocloud.domain.Taco;
@@ -49,5 +50,13 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign (Taco design){
+        //save the taco design
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 }
