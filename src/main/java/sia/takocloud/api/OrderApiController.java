68e9ebaf6db3b98ea1c0a98b8687c6a44c017ba5
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-07-15 12:04:25 +0200

    Added methods to see all orders, post an order,  patch an order and delete it.

--- src/main/java/sia/takocloud/api/OrderApiController.java
+++ src/main/java/sia/takocloud/api/OrderApiController.java
@@ -1,5 +1,6 @@
 package sia.takocloud.api;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sia.takocloud.domain.Order;
@@ -27,12 +28,43 @@ public class OrderApiController {
         return orderRepository.save(order);
     }
 
-    /*@PatchMapping(path = "/{orderId}", consumes = "application/json")
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
     public Order patchOrder(@PathVariable("orderId") Long orderId, @RequestBody Order patch){
 
         Order order = orderRepository.findById(orderId).get();
 
-        if (patch.get)
+        if (patch.getName() != null){
+            order.setName(patch.getName());
+        }
+        if (patch.getStreet() != null){
+            order.setStreet(patch.getStreet());
+        }
+        if (patch.getCity() != null){
+            order.setCity(patch.getCity());
+        }
+        if (patch.getState() != null){
+            order.setState(patch.getState());
+        }
+        if (patch.getZip() != null){
+            order.setZip(patch.getZip());
+        }
+        if (patch.getCcNumber() != null){
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null){
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null){
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(order);
+    }
 
-    }*/
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") Long orderId){
+        try {
+            orderRepository.deleteById(orderId);
+        }catch (EmptyResultDataAccessException e){ }
+    }
 }
